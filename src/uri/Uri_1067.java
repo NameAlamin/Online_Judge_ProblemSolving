8b4ff42f2dd7c427161837949adc6552a773dd39
Alamin <alamin8740@gmail.com>
2022-04-26 23:30:16 +0600

    uri 1067

--- src/uri/Uri_1067.java
+++ src/uri/Uri_1067.java
@@ -1,5 +1,18 @@
 package uri;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Uri_1067 {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int x,i;
+        x = Integer.parseInt(reader.readLine());
+        for(i=1;i<=x;i++){
+            if(i%2 != 0){
+                System.out.println(i);
+            }
+        }
+    }
 }
